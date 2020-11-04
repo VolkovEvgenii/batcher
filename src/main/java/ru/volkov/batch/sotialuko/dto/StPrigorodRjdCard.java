6996637bf0f12dcff09d8be7c658a1f8df239cfa
evgeniy volkov <volkov.e.n@gmail.com>
2020-11-04 16:30:58 +0300

    Replace StPrigorodRjdCard

---
+++ src/main/java/ru/volkov/batch/sotialuko/dto/StPrigorodRjdCard.java
@@ -0,0 +1,10 @@
+package ru.volkov.batch.sotialuko.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class StPrigorodRjdCard implements Serializable {
+    public String card_bsk_num;
+}
--- src/main/java/ru/volkov/batch/util/db/StPrigorodRjdCard.java
+++
@@ -1,8 +0,0 @@
-package ru.volkov.batch.util.db;
-
-import lombok.Data;
-
-@Data
-public class StPrigorodRjdCard {
-    public String card_bsk_num;
-}
